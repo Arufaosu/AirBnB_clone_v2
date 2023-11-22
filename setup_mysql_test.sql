c9f9a490e5b23fe77a1ffea196b03b537c136cce
root <RobinLeeHenry101@gmail.com>
2023-11-22 05:37:25 -0800

    fixed the sql test file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,21 +1,10 @@
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    passwd='your_root_password'
-)
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-cursor = db.cursor()
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_test_db")
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'")
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
-cursor.execute("GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'")
-
-cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'")
-
-cursor.execute("FLUSH PRIVILEGES")
-
-cursor.close()
-db.close()
+FLUSH PRIVILEGES;
