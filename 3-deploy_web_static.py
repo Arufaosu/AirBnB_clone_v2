8c489e79d7dbfc84907d38f013737b785ba42e97
root <RobinLeeHenry101@gmail.com>
2023-12-06 09:12:53 -0800

    trying to fix task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python3
-from fabric.api import env, local
-from os.path import exists
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+from fabric.api import local, env, run, put
+from datetime import datetime
+import os
 
 env.hosts = ['<IP web-01>', '<IP web-02>']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/my_ssh_private_key'
 
-def deploy():
+def do_pack():
     """ creates and distributes an archive to your web servers """
+    try:
+        local("mkdir -p versions")
+        timestr = datetime.now().strftime("%Y%m%d%H%M%S")
+        path = "versions/web_static_{}.tgz".format(timestr)
+        local("tar -cvzf {} web_static".format(path))
+        return path
+    except Exception as e:
+        return None
+
+def do_deploy(archive_path):
+    """ distributes archive """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        filename = archive_path.split("/")[-1]
+        path_no_ext = "/data/web_static/releases/{}".format(
+            filename.replace(".tgz", ""))
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}/web_static".format(path_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path_no_ext))
+        return True
+    except Exception as e:
+        return False
+
+def deploy():
+    """ full deployment """
     archive_path = do_pack()
     if not archive_path:
         return False
     return do_deploy(archive_path)
 
+
 if __name__ == "__main__":
-    result = deploy()
-    if result:
-        print("Deployment successful!")
-    else:
-        print("Deployment failed.")
+    deploy()
